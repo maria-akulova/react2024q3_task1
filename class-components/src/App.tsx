11aad68469c9d7b812c1f24683a1273615da4a0c
maria.akulova <maria.akulova.dragon@gmail.com>
2024-07-15 01:44:23 +0200

    add routing and detail page

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,44 +1,20 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Animal, InputSearch, ResultSearch, Spinner } from './components/index.ts';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Animals } from './pages/animals/Animals';
+import { NotFound } from './pages/notfound/NotFound';
 import './App.css';
-import api from './services/api.ts';
-import { useSearchQuery } from './hooks/useSearchQuery';
+import { AnimalDetails } from './components/details/AnimalDetails';
 
 const App: React.FC = () => {
-  const [animals, setAnimals] = useState<Animal[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [searchQuery] = useSearchQuery('');
-
-  const getAnimals = useCallback(async (searchTerm: string) => {
-    setLoading(true);
-    try {
-      const res = await api.getAnimals(searchTerm);
-      setAnimals(res.animals);
-      setLoading(false);
-    } catch (err) {
-      console.error('Failed to fetch animals', err);
-      setLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    getAnimals(searchQuery);
-  }, [getAnimals, searchQuery]);
-
-  if (error) {
-    throw new Error('Test Error');
-  }
-
   return (
-    <>
-      <h1 className="header">Animals</h1>
-      <InputSearch onSearch={getAnimals} />
-      <button className="errorButton" onClick={() => setError(true)}>
-        Throw Test Error
-      </button>
-      {loading ? <Spinner /> : <ResultSearch animals={animals} />}
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/*" element={<Animals />}>
+          <Route path="details/:id" element={<AnimalDetails />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
 
--- class-components/src/components/mainsection/ResultSearch.tsx
+++ class-components/src/components/mainsection/ResultSearch.tsx
@@ -4,33 +4,30 @@ import { Animal } from 'components';
 
 interface ResultSearchProps {
   animals: Animal[];
+  onItemClick: (id: string) => void;
 }
 
-export const ResultSearch: React.FC<ResultSearchProps> = ({ animals }) => {
-  const getAnimalType = (animal: Animal): string => {
-    if (animal.avian) return 'Avian';
-    if (animal.canine) return 'Canine';
-    if (animal.earthAnimal) return 'Earth Animal';
-    if (animal.earthInsect) return 'Earth Insect';
-    if (animal.feline) return 'Feline';
-    return 'not defined by default';
-  };
-
+export const ResultSearch: React.FC<ResultSearchProps> = ({ animals, onItemClick }) => {
   return (
     <>
       <section className={style.section}>
         {animals.length === 0 && (
           <div className={style.noResults}>No results. Try another name.</div>
         )}
-        {animals.map((animal) => (
-          <div key={animal.uid} className={style.animal}>
-            <p>{animal.name}</p>
-            <div className={style.description}>
-              <p>Unique number: {animal.uid}</p>
-              <p>The Type is {getAnimalType(animal)}</p>
+        <div>
+          {animals.map((animal) => (
+            <div
+              key={animal.uid}
+              className={style.animal}
+              onClick={(e) => {
+                e.stopPropagation();
+                onItemClick(animal.uid);
+              }}
+            >
+              <p>{animal.name}</p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
     </>
   );
