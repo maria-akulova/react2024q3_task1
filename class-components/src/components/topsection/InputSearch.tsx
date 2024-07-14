68bd8c54ef0fb56edee2710cd63d3bb27f84635b
Maria <99416741+maria-akulova@users.noreply.github.com>
2024-07-14 23:51:55 +0200

    feat: convert Component to function (#13)

--- class-components/src/components/topsection/InputSearch.tsx
+++ class-components/src/components/topsection/InputSearch.tsx
@@ -1,56 +1,46 @@
-import { Component, ChangeEvent, MouseEvent } from 'react';
+import React, { ChangeEvent, MouseEvent } from 'react';
 import style from './InputSearch.module.scss';
 import { trunc } from '../../utils/HelperString.ts';
+import { useSearchQuery } from '../../hooks/useSearchQuery';
 
 interface InputSearchProps {
   onSearch: (searchTerm: string) => void;
 }
 
-export class InputSearch extends Component<InputSearchProps> {
-  state = {
-    searchTerm: '',
+export const InputSearch: React.FC<InputSearchProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useSearchQuery('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
   };
 
-  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-
-  handleSearch = (event: MouseEvent<HTMLButtonElement>) => {
+  const handleSearch = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    const searchName = trunc(this.state.searchTerm);
-    this.setState({ searchTerm: searchName });
-    this.props.onSearch(searchName);
-    localStorage.setItem('search', searchName);
+    const searchName = trunc(searchTerm);
+    setSearchTerm(searchName);
+    onSearch(searchName);
   };
 
-  componentDidMount(): void {
-    const historySearch: string | null = localStorage.getItem('search');
-    const result = historySearch ? historySearch : '';
-    this.setState({ searchTerm: result });
-  }
-
-  render() {
-    return (
-      <>
-        <label className={style.searchLabel} htmlFor="searchInput">
-          Search by Name of animal
-        </label>
-        <section>
-          <input
-            className={style.input}
-            type="text"
-            id="searchInput"
-            name="searchInput"
-            placeholder="Type to search..."
-            value={this.state.searchTerm}
-            onChange={this.handleInputChange}
-            maxLength={30}
-          />
-          <button className={style.searchButton} onClick={this.handleSearch}>
-            Search
-          </button>
-        </section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <label className={style.searchLabel} htmlFor="searchInput">
+        Search by Name of animal
+      </label>
+      <section>
+        <input
+          className={style.input}
+          type="text"
+          id="searchInput"
+          name="searchInput"
+          placeholder="Type to search..."
+          value={searchTerm}
+          onChange={handleInputChange}
+          maxLength={30}
+        />
+        <button className={style.searchButton} onClick={handleSearch}>
+          Search
+        </button>
+      </section>
+    </>
+  );
+};
