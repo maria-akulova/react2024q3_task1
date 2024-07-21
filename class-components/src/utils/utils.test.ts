d5c2889b510e7f76532eaf3645959c9aeb5fcb11
Maria <99416741+maria-akulova@users.noreply.github.com>
2024-07-21 19:07:51 +0200

    Vitest (#18)
    * add vitest, test coverage report and tests for utils
    
    * add pre-push script
    
    * add suite and ui report

---
+++ class-components/src/utils/utils.test.ts
@@ -0,0 +1,92 @@
+import { describe, expect, test } from 'vitest';
+import { getAnimalType, trunc } from './HelperString';
+
+describe('Utils', () => {
+  test('Trim string with spaces around', () => {
+    expect(trunc('  test trunc  ')).toBe('test trunc');
+  });
+
+  const avianAnimal = {
+    avian: true,
+    canine: false,
+    earthAnimal: false,
+    earthInsect: false,
+    feline: false,
+    name: 'test',
+    uid: 'avian_uid',
+  };
+
+  test('Check types: avian', () => {
+    expect(getAnimalType(avianAnimal)).toBe('Avian');
+  });
+
+  const canineAnimal = {
+    avian: false,
+    canine: true,
+    earthAnimal: false,
+    earthInsect: false,
+    feline: false,
+    name: 'test',
+    uid: 'canine_uid',
+  };
+
+  test('Check types: canine', () => {
+    expect(getAnimalType(canineAnimal)).toBe('Canine');
+  });
+
+  const earthAnimal = {
+    avian: false,
+    canine: false,
+    earthAnimal: true,
+    earthInsect: false,
+    feline: false,
+    name: 'test',
+    uid: 'earth_uid',
+  };
+
+  test('Check types: earth animal', () => {
+    expect(getAnimalType(earthAnimal)).toBe('Earth Animal');
+  });
+
+  const earthInsect = {
+    avian: false,
+    canine: false,
+    earthAnimal: false,
+    earthInsect: true,
+    feline: false,
+    name: 'test',
+    uid: 'earthInsect_uid',
+  };
+
+  test('Check types: earth Insect', () => {
+    expect(getAnimalType(earthInsect)).toBe('Earth Insect');
+  });
+
+  const felineAnimal = {
+    avian: false,
+    canine: false,
+    earthAnimal: false,
+    earthInsect: false,
+    feline: true,
+    name: 'test',
+    uid: 'felin_uid',
+  };
+
+  test('Check types: feline ', () => {
+    expect(getAnimalType(felineAnimal)).toBe('Feline');
+  });
+
+  const underfinedType = {
+    avian: false,
+    canine: false,
+    earthAnimal: false,
+    earthInsect: false,
+    feline: false,
+    name: 'test',
+    uid: 'felin_uid',
+  };
+
+  test('Check when type is uknown:  ', () => {
+    expect(getAnimalType(underfinedType)).toBe('not defined by default');
+  });
+});
--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +7,14 @@ export default defineConfig({
   base: './',
 
   plugins: [react()],
+  esbuild: {
+    include: ['**/*.js', '**/*.jsx', '**/*.mjs', '**/*.ts', '**/*.tsx'],
+  },
+  test: {
+    watch: false,
+    coverage: {
+      provider: 'v8',
+      ignoreEmptyLines: true,
+    },
+  },
 });
